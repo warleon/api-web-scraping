ece3a4476a0e9aa2f8d0440b698dfd16ecce1fc4
warleon <51892390+warleon@users.noreply.github.com>
2025-11-23 13:32:37 -0500

    with layer

--- scrap_table.py
+++ scrap_table.py
@@ -5,32 +5,23 @@ import time
 import boto3
 
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import ChromeOptions
 
 
 def get_driver():
-    chrome_options = Options()
+    chrome_options = ChromeOptions()
     chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--single-process")
-    chrome_options.add_argument("--window-size=1920,1080")
-    chrome_options.add_argument(
-        "--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/120.0.0.0 Safari/537.36"
-    )
-
-    chrome_options.binary_location = "/opt/chrome/chrome"
+    chrome_options.add_argument("--window-size=1280x1696")
+    chrome_options.binary_location = "/opt/chromium"
 
     service = Service("/opt/chromedriver")
 
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    return driver
+    return webdriver.Chrome(service=service, options=chrome_options)
 
 
 def lambda_handler(event, context):
